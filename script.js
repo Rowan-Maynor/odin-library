490e735a9becf24988eb3c903565fefc1ad9b27b
Rowan Maynor <rowanmaynor@gmail.com>
2024-12-03 06:25:39 -0500

    Create hasReadToggle

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ function createBookCard(id, title, description) {
     createBookDiv(id);
     createBookTitle(id, title);
     createBookDescription(id, description);
-    //create has not read p
-    //attach to div
+    createHasReadToggle(id);
     //add on click event for has not read p to toggle
     //create remove book p
     //attach to div
@@ -43,6 +42,25 @@ function createBookDescription(id, description) {
     const newP = document.createElement("p");
     newP.classList = "book-section book-description";
     newP.textContent = `${description}`;
+    const cardDiv = document.getElementById(`${id}`);
+    cardDiv.appendChild(newP);
+}
+
+function createHasReadToggle(id) {
+    const newP = document.createElement("p");
+    newP.classList = "book-section book-read-toggle-false";
+    newP.textContent = "Has Not Read";
+
+    newP.addEventListener("click", function(){
+        if(this.classList=="book-section book-read-toggle-false"){
+            this.classList="book-section book-read-toggle-true";
+            this.textContent="Has Read"
+        } else {
+            this.classList="book-section book-read-toggle-false";
+            this.textContent="Has Not Read"
+        }
+    });
+
     const cardDiv = document.getElementById(`${id}`);
     cardDiv.appendChild(newP);
 }
